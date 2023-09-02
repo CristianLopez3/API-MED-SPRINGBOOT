e9050722849d954e2ca5c5b8857f3870d758e7b0
Cristian-Lopez <126030346+CristianLopez3@users.noreply.github.com>
2023-09-01 22:17:36 -0500

    try with 400 exception in infra.ExceptionHandler

--- src/main/java/med/voll/api/infra/ExceptionsHandler.java
+++ src/main/java/med/voll/api/infra/ExceptionsHandler.java
@@ -3,6 +3,8 @@ package med.voll.api.infra;
 
 import jakarta.persistence.EntityNotFoundException;
 import org.springframework.http.ResponseEntity;
+import org.springframework.validation.FieldError;
+import org.springframework.web.bind.MethodArgumentNotValidException;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.RestControllerAdvice;
 
@@ -26,5 +28,20 @@ public class ExceptionsHandler {
     public ResponseEntity tryError404(){
         return ResponseEntity.notFound().build();
     }
+
+
+    @ExceptionHandler(MethodArgumentNotValidException.class)
+    public ResponseEntity tryError400(MethodArgumentNotValidException e){
+
+        var errors = e.getFieldErrors().stream().map(DataErrorValidation::new).toList();
+        return ResponseEntity.badRequest().body(errors);
+    }
+
+    private record DataErrorValidation(String field, String error){
+        public DataErrorValidation(FieldError error){
+            this(error.getField(), error.getDefaultMessage());
+        }
+    }
+
 
 }
